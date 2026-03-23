f8a98296b106c06137f83d50ccbc68419a187e80
Emi <118975094+fridasola@users.noreply.github.com>
2026-03-23 07:56:17 +0100

    Implement solve2 function for Hanoi problem
    En observant le mouvement du plus petit disque, et en remarquant qu’il n’y a qu’un seul mouvement possible lorsque le plus petit disque ne bouge pas, construisez un algorithme non récursif pour résoudre le problème

--- HanoiCompleter.py
+++ HanoiCompleter.py
@@ -32,3 +32,27 @@ def solve (n ,A ,B , C ):
   if n ==1: 
     return [( A , B )] 
   return solve (n -1 ,A ,C , B )+[( A , B )]+ solve (n -1 ,C ,B , A )
+#5
+def solve2 (H , N ): 
+  """ 
+  Hanoi object x int -> résout le problème des tours de Hanoï ( pour H ) avec N disques 
+  """ 
+  petit = 0 
+  if N %2==0: 
+    sens = 1
+  else : 
+    sens = 2  
+  H . move ( petit , petit + sens ) 
+  petit = petit + sens 
+  for n in range ((2** N -2)//2):
+    try : 
+      H . move (( petit +1)%3 ,( petit +2)%3) 
+    except : 
+      H . move (( petit +2)%3 ,( petit +1)%3) 
+    H . move ( petit ,( petit + sens )%3) 
+    petit = ( petit + sens )%3
+    
+H = Hanoi (5 ,0.5) 
+solve2 (H ,5) 
+H = Hanoi (10 ,0.01) 
+solve2 (H ,10)
