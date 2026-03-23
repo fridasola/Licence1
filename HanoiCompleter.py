17d209c4629dbb04f25ff3ff0ee929e404ae5033
Emi <118975094+fridasola@users.noreply.github.com>
2026-03-23 07:48:18 +0100

    Implement Tower of Hanoi solver in HanoiCompleter.py
    fonction python solve(H,n,A,B,C), où H est l’objet python retourné par Hanoi(), n le nombre de disques à déplacer, et A,B,C trois entiers distincts dans {0, 1, 2} repérant respectivement la tige de départ, la tige d’arrivée et la tige restante comme dans la question précédente.

--- HanoiCompleter.py
+++ HanoiCompleter.py
@@ -1,3 +1,5 @@
+#Exercice 26
+#1
 from hanoi_tkinter import Hanoi 
 H = Hanoi (3 ,1.5)
 H . move (0 ,2) 
@@ -7,3 +9,18 @@ H . move (0 ,2)
 H . move (1 ,0) 
 H . move (1 ,2) 
 H . move (0 ,2)
+#3
+def solve (H ,n ,A ,B , C ): 
+  """ 
+  Hanoi object x int x int x int x int -> déplace les n disques supérieurs de la tige A vers la tige B ( C est la tige restante ) 
+  """ 
+  if n ==1: 
+    H . move (A , B ) 
+  else : 
+    solve (H ,n -1 ,A ,C , B ) 
+    H . move (A , B ) 
+    solve (H ,n -1 ,C ,B , A ) 
+H = Hanoi (5 ,0.5) 
+solve (H ,5 ,0 ,2 ,1) 
+H = Hanoi (10 ,0.01) 
+    solve (H ,10 ,0 ,2 ,1)
